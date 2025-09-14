b6d51266c896c65c628f7a000cbdcb43b4116eb9
VxpeInjected <209692344+VxpeInjected@users.noreply.github.com>
2025-09-14 17:03:51 +0100

    Update Group-Blacklist.lua

--- Roblox/Roblox Studios/Luau Scripts/Group Blacklist/Group-Blacklist.lua
+++ Roblox/Roblox Studios/Luau Scripts/Group Blacklist/Group-Blacklist.lua
@@ -1,1 +1,16 @@
+local blacklistedGroups = {1, 2}; -- Group IDs here.
+local groupService = game:GetService('GroupService');
+
+game:GetService('Players').PlayerAdded:Connect(function(player)
+	local playerGroupInfo = groupService:GetGroupsAsync(player.UserId);
+
+	for _, selected in pairs(playerGroupInfo) do
+		for i, banned in pairs(blacklistedGroups) do
+			if selected.Id == banned then
+				player:Kick('\nSystem:\n\nYou are in groups that are on our blacklist. To continue playing, you must leave this group.\n\nThe group(s) name is:\n'..selected.Name);
+			end;
+		end;
+	end;
+end);
+
 
