fd525de45b1eb1dddf4511a9e8591e78f1329420
VxpeInjected <209692344+VxpeInjected@users.noreply.github.com>
2025-09-14 16:38:19 +0100

    Update FOV-Changer.lua

--- Roblox/Roblox Studios/Luau Scripts/FOV Changer/FOV-Changer.lua
+++ Roblox/Roblox Studios/Luau Scripts/FOV Changer/FOV-Changer.lua
@@ -1,1 +1,6 @@
+local player = game.Players.LocalPlayer
+local camera = workspace.CurrentCamera
+camera.FieldOfView = 70
 
+-- Change the number in 𝗹𝗶𝗻𝗲 𝟯 to whatever you want the standard FOV to be.
+-- The standard FOV for a normal game is 70.
